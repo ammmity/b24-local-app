690f7f20ca8323a6d7c14d0da48365691ded7ae4
Andrey <ammmity@gmail.com>
2025-02-26 00:36:27 +0300

    feature: Добавить в модель деталь привязку к этапам производства для комплектующих

--- src/Entities/ProductPart.php
+++ src/Entities/ProductPart.php
@@ -2,11 +2,15 @@
 
 namespace App\Entities;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity, Table(name: 'product_parts')]
 class ProductPart
@@ -23,14 +27,26 @@ class ProductPart
     #[Column(type: 'integer', unique: false, nullable: false)]
     private $bitrix_id;
 
+    #[OneToMany(targetEntity: ProductProductionStage::class, mappedBy: 'productPart')]
+    private Collection $productionStages;
+
     public function toArray(): array
     {
-        return [
+        $data = [
             'id' => $this->getId(),
             'name' => $this->getName(),
             'xml_id' => $this->getXmlId(),
             'bitrix_id' => $this->getBitrixId(),
         ];
+
+        // Добавляем этапы производства, если они есть
+        if (!$this->productionStages->isEmpty()) {
+            $data['production_stages'] = $this->productionStages
+                ->map(fn(ProductProductionStage $stage) => $stage->toArray())
+                ->toArray();
+        }
+
+        return $data;
     }
 
     // Геттеры и сеттеры
@@ -70,6 +86,36 @@ class ProductPart
     public function setBitrixId(int $bitrix_id): self
     {
         $this->bitrix_id = $bitrix_id;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ProductProductionStage>
+     */
+    public function getProductionStages(): Collection
+    {
+        return $this->productionStages;
+    }
+
+    public function addProductionStage(ProductProductionStage $stage): self
+    {
+        if (!$this->productionStages->contains($stage)) {
+            $this->productionStages->add($stage);
+            $stage->setProductPart($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProductionStage(ProductProductionStage $stage): self
+    {
+        if ($this->productionStages->removeElement($stage)) {
+            // set the owning side to null (unless already changed)
+            if ($stage->getProductPart() === $this) {
+                $stage->setProductPart(null);
+            }
+        }
+
         return $this;
     }
 }
